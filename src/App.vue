476ebdb1709e32d9f771d66b74ea269740ad9fbe
Pablo Ferreiro <pferreiromero@gmail.com>
2021-01-20 22:22:04 +0100

    hotfix

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,75 @@
 <template>
   <div id="app">
     <Navbar></Navbar>
-    <router-view/>
+    <router-view />
     <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
-import { hasPredifinedURL } from './services/api.js'
-import { startup } from './services/common.js'
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
+import { hasPredifinedURL } from "./services/api.js";
+import { startup } from "./services/common.js";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Navbar,
     Footer
   },
-  beforeCreate: async function() {
+  beforeCreate: async function () {
     // Initial setup
     let server;
     // Check if URL is hardcoded in .env
     if (!hasPredifinedURL()) {
       // Did user already finished the setup?
       if (!localStorage.servers || !this.$store.state.servers) {
-        this.$router.push('/config')
+        this.$router.push("/config");
+      } else {
+        this.$store.commit("setServers", JSON.parse(localStorage.servers));
+        server = this.$store.state.servers.active;
       }
-      else {
-        this.$store.commit('setServers', JSON.parse(localStorage.servers))
-        server = this.$store.state.servers.active
-      }
+    } else {
+      server = process.env.VUE_APP_SERVER;
     }
-    else {
-      server = process.env.VUE_APP_SERVER
-    }
-    
+
     // Ping server
-    const serverinfo = await startup(server)
-    if (serverinfo) {
-      // Check if cookie didn't expire (TODO, refresh tokens)
-      if (!serverinfo.loggedin && localStorage.loggedin) {
-        this.$store.commit('setLoggedin', false)
-        this.$store.commit('setUserinfo', null)
-        localStorage.removeItem("loggedin")
-        localStorage.removeItem("userinfo")
-        this.$buefy.toast.open("Tu sesión ha caducado, inicie sesión otra vez")
+    if (server) {
+      const serverinfo = await startup(server);
+      if (serverinfo) {
+        // Check if cookie didn't expire (TODO, refresh tokens)
+        if (!serverinfo.loggedin && localStorage.loggedin) {
+          this.$store.commit("setLoggedin", false);
+          this.$store.commit("setUserinfo", null);
+          localStorage.removeItem("loggedin");
+          localStorage.removeItem("userinfo");
+          this.$buefy.toast.open(
+            "Tu sesión ha caducado, inicie sesión otra vez"
+          );
+        }
+      } else {
+        // Set server config
+        this.$buefy.toast.open({
+          duration: 5000,
+          message: `Ha habido un error al contactar con el servidor remoto`,
+          position: "is-bottom",
+          type: "is-danger",
+        });
       }
-    }
-    else {
-      // Set server config
-      this.$buefy.toast.open({
-        duration: 5000,
-        message: `Ha habido un error al contactar con el servidor remoto`,
-        position: 'is-bottom',
-        type: 'is-danger'
-      })
     }
 
     // Is user already logged in?
     if (localStorage.loggedin) {
-      const userinfo = JSON.parse(localStorage.userinfo)
-      this.$store.commit('setUserinfo', userinfo)
-      this.$store.commit('setLoggedin', localStorage.loggedin)
+      const userinfo = JSON.parse(localStorage.userinfo);
+      this.$store.commit("setUserinfo", userinfo);
+      this.$store.commit("setLoggedin", localStorage.loggedin);
       if (localStorage.guardianinfo) {
-        const guardianinfo = JSON.parse(localStorage.guardianinfo)
-        this.$store.commit('setGuardianinfo', guardianinfo)
+        const guardianinfo = JSON.parse(localStorage.guardianinfo);
+        this.$store.commit("setGuardianinfo", guardianinfo);
       }
     }
-  }
-}
+  },
+};
 </script>
 
 <style>
